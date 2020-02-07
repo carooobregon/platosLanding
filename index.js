7e9b726525f97ee2b78a64b6cd5d01f4c2b3f4d1
carooobregon <caroobregon@gmail.com>
2020-02-06 19:14:14 -0700

    implemented owl carousel

--- index.js
+++ index.js
@@ -22,3 +22,28 @@ $("#platos").click(function(){
  	$(".platosDesc").css("display", "inline");
    $("#platos").css("font-weight", "700");
  });
+
+ $(document).ready(function(){
+	$(".owl-carousel").owlCarousel();
+  });
+  
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
